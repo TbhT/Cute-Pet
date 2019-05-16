4d52d3fcb467d9f6508913576359043a9d5dcd8d
archivewang <archivewang@upchina.com>
2019-05-16 18:07:45 +0800

    feat: 添加swiper

--- src/js/routes.all.js
+++ src/js/routes.all.js
@@ -1,10 +1,11 @@
 import TweetsList from '../pages/tweets.f7.html'
+import Slider from '../pages/swiper-slider.f7.html'
 
 
 
 export default [
   {
     path: '/',
-    component: TweetsList
+    component: Slider
   }
 ]
