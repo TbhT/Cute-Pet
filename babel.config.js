86c387edfead1c9b9125ee9bf8ead678371e7715
archivewang <archivewang@upchina.com>
2019-05-17 16:11:27 +0800

    feat: add js

--- babel.config.js
+++ babel.config.js
@@ -16,7 +16,7 @@ module.exports = {
     }],
   ],
   plugins: [
-    // "@babel/plugin-transform-runtime",
+    '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
   ],
 };
