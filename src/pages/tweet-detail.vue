772f8826d7e27b859345d4999d2df568efac0b99
archivewang <archivewang@upchina.com>
2019-05-21 18:23:39 +0800

    feat: 添加tweet详情页

---
+++ src/pages/tweet-detail.vue
@@ -0,0 +1,51 @@
+<template>
+  <f7-page class="me-tweet-detail">
+    <f7-navbar :back-link="backText" sliding></f7-navbar>
+  
+    <card :enableToolbar="false" :data="post"></card>
+
+    <div class="me-comments">
+      <div class="me-title">
+        <span>评论</span>
+      </div>
+
+      <div class="me-comments-list" v-if="comments.length">
+
+      </div>
+
+      <div class="me-no-comment" v-else>
+        <i class="iconfont icon-wujieguoyangshi"></i>
+        <div class="me-text">
+          <span>暂无内容</span>
+        </div>
+      </div>
+    </div>
+
+    <f7-toolbar class="me-flex-row" position="bottom">
+      
+    </f7-toolbar>
+  </f7-page>
+</template>
+
+<script>
+import Card from '../pages/card.vue'
+
+
+export default {
+  components: {
+    Card
+  },
+  props: {
+    post: {}
+  },
+  data() {
+    return {
+      backText: '返回'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
