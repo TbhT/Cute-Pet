7e828efe16f1961c21b83ff4dadfda58c1d3a516
TbhT <hustapple132@gmail.com>
2019-05-27 18:51:29 +0800

    feat: 添加date-time组件

---
+++ src/components/date-time.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="item-content item-input">
+    <div class="item-inner">
+      <div class="item-input-wrap">
+        <input type="text" :placeholder="placeholder" readonly="readonly" :id="elementId">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    elementId: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+    this.$f7.picker.create({
+      inputEl: this.elementId,
+      toolbar: false,
+      rotateEffect: true,
+      value: [
+        
+      ]
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
