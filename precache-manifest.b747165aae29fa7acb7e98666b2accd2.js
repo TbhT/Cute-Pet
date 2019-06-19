7c7c0076512affd048f674778615ed3678c4b43d
TbhT <hustapple132@gmail.com>
2019-06-20 00:33:04 +0800

    feat: 增加静态页面

---
+++ precache-manifest.b747165aae29fa7acb7e98666b2accd2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8df6867f9246f6fd7caaa377cc93df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d95678f07063b294778",
+    "url": "1.app.js"
+  },
+  {
+    "revision": "5e89298e7c2f72c5d3c3",
+    "url": "app.js"
+  },
+  {
+    "revision": "9d95678f07063b294778",
+    "url": "css/1.app.css"
+  },
+  {
+    "revision": "5e89298e7c2f72c5d3c3",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "589f22db3ab56eb2cfc548d104f6d6f7",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "48c1c71096585974d7ed087615202945",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "0efb066cbbeaa761f76a062f425e5af9",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "988e8c7a7c9a409cf58e6d72c9d73996",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "9c1f6e6d8fa8543ed98082f3f2650416",
+    "url": "fonts/iconfont.eot"
+  },
+  {
+    "revision": "3940bb84afb256e7cefb077985865f70",
+    "url": "fonts/iconfont.ttf"
+  },
+  {
+    "revision": "a09903d6bb0375d3ce816f7d46403e45",
+    "url": "images/iconfont.svg"
+  },
+  {
+    "revision": "56261db7e33b570f3d3e4daa79dede60",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b08f600e75284d6274ee0ee03f2ed1b3",
+    "url": "static/icons/128x128.png"
+  },
+  {
+    "revision": "78bb1bad56b56f9176976fbb19f640d8",
+    "url": "static/icons/144x144.png"
+  },
+  {
+    "revision": "09bac65cbc73f494482c480006f7c31a",
+    "url": "static/icons/152x152.png"
+  },
+  {
+    "revision": "1c41296078623fe115c3b966852a9759",
+    "url": "static/icons/192x192.png"
+  },
+  {
+    "revision": "ad809d0a19d880fc189e41e9726df58e",
+    "url": "static/icons/256x256.png"
+  },
+  {
+    "revision": "9fa40ac188452505f216044f8c7a9b88",
+    "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "14235e3018bb57cc700c799c5f9ef1be",
+    "url": "static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "c88219dd9796cca0bfa90e472a805ab9",
+    "url": "static/icons/favicon.png"
+  }
+]);
